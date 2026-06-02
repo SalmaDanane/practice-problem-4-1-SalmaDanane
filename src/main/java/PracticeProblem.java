7acf48dcc730ad3eacc9e0a740c5a3e0e8807d89
SalmaDanane <343249595@tdsb.ca>
2026-06-02 10:38:09 -0400

    Update PracticeProblem.java
    sakm

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,27 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
 
-	public static void q1() {
+	public static String seriesInclusive(int num) {
 		//Write question 1 code here
+		int i = 1;
+		String series = "";
+		while (i <= num) {
+			series = series + i + " ";
+			i=i+1;
+		}
+		return series.trim();
 	}
 
-	public static void q2() {
+	public static String seriesExclusive(int num) {
 		//Write question 2 code here
+		int i = 0;
+		String series = "";
+		while (i < num) {
+			series = series + i + " ";
+			i=i+1;
+		}
+		return series.trim();
 	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
